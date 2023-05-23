77aceb57122413778f3e0057a387a7e76d1e1735
shahabuddinhossain <voc_1992@yahoo.com>
2023-05-23 11:38:27 +0600

    All problem solved

--- assignment14/app/Http/Controllers/WebController.php
+++ assignment14/app/Http/Controllers/WebController.php
@@ -7,6 +7,26 @@ use Illuminate\Http\Request;
 class WebController extends Controller
 {
     function return_name (Request $request){
-        $request->input('name');
+        $name       =   $request->input('name');
+        $userAgent  =   $request->header('user-agent');
+        $page       =   $request->query('page', null);
+        $rememberToken = $request->cookie('remember_token', null);
+
+
+        return response()->json([
+            'message' => 'Success',
+            'data' => [
+                'name' => 'John Doe',
+                'age' => 25
+            ]
+        ]);
+
+        if ($request->hasFile('avatar')) {
+            $file = $request->file('avatar');
+            $fileName = $file->getClientOriginalName();
+            $file->move('public/uploads', $fileName);
+        }
+
     }
 }
+
